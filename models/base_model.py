a0fca20d18a4bc125dc5d046c64ed29471e1ed50
phil-droid <akkoli058@gmail.com>
2023-04-28 07:48:21 -0700

    huuh

--- models/base_model.py
+++ models/base_model.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+
+import hashlib
+import os
 from sqlalchemy import Column, DateTime, String
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
@@ -9,10 +13,24 @@ class BaseModel:
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow)
 
-
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
-            setattr(self, key, value)
+            if key == 'password':
+                self.password = self.__hash_password(value)
+            else:
+                setattr(self, key, value)
+
+    def __hash_password(self, password):
+        """Hashes a password using MD5 and a salt"""
+        salt = os.urandom(32).hex()
+        hashed_password = hashlib.md5((password + salt).encode()).hexdigest()
+        return salt + hashed_password
+
+    def verify_password(self, password):
+        """Verifies a password against the hashed password"""
+        salt = self.password[:64]
+        hashed_password = hashlib.md5((password + salt).encode()).hexdigest()
+        return self.password[64:] == hashed_password
 
     def save(self):
         self.updated_at = datetime.utcnow()
@@ -21,8 +39,13 @@ class BaseModel:
 
     def delete(self):
         models.storage.delete(self)
-    
-    def to_dict(self):
-        new_dict = self.__dict__.copy()
-        new_dict.pop('_sa_instance_state', None)
+
+    def to_dict(self, **kwargs):
+        new_dict = {}
+        for key, value in self.__dict__.items():
+            if key == 'password' and 'password' not in kwargs:
+                continue
+            if key == '_sa_instance_state':
+                continue
+            new_dict[key] = value
         return new_dict
